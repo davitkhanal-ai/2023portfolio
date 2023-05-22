c85b5540183f10ac1766e7a3c7679b68563ae1e9
davit khanal <khanaldavit@gmail.com>
2023-05-23 01:12:33 +0545

    Remove  Career dialogue box after deployed in netlify

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,7 @@
           </section>
 
           {/* second page */}
+          {/* career */}
           <section>
           <div>
             <h3 className="text-3xl py-1 ">Career</h3>
@@ -73,6 +74,7 @@
             </p>
           </div>
           <div className="lg:flex gap-12">
+
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
@@ -85,37 +87,10 @@
               <p className="text-gray-800 py-1">Data collection and database management for nepali sports personnel and officials</p>
               
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
-              </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
+
           </div>
         </section>
-       
+        {/* career end */}
 
         </main>
       </div>
