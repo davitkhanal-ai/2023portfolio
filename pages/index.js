e8e404cc1e325c01e185770350f1515a5addc97b
davitkhanal <46323410+davitkhanal-ai@users.noreply.github.com>
2023-06-25 19:23:35 +0545

    hide nav bar

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@
           <section className='min-h-screen'>
 
        {/* navigation */}
-            <nav className='py-10 mb-12 flex
+            {/* <nav className='py-10 mb-12 flex
             justify-between'>
               <h1 className='text-xl font-burtons dark:text-white'>Davit AI</h1>
               <ul className='flex items-center'>
@@ -42,7 +42,7 @@
                   <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
                   </li>
               </ul>
-            </nav>
+            </nav> */}
 
         {/* display page */}
           <div className='text-center p-5 py-2'>
